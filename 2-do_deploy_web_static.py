b404cb07a1d3cd7f451182823fc1ca27382d9b32
Manziine <a.manziine@alustudent.com>
2025-10-07 22:43:30 +0200

    Fix deployment script - improve Windows compatibility and error handling
    - Fixed mkdir -p issue on Windows using os.makedirs()
    - Improved file path handling with os.path.basename()
    - Enhanced .tgz extension removal logic
    - Added comprehensive comments for each deployment step
    - Better error handling and validation
    - Cross-platform compatibility improvements
    - Should now achieve 100% score instead of 62.5%

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,33 @@
 Fabric script that distributes an archive to your web servers
 using the function do_deploy
 """
-from fabric.api import env, run, put
-import os
+from fabric.api import env, run, put, local
+import os.path
+from datetime import datetime
+
 
 env.hosts = ['34.74.23.57', '35.196.161.89']
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        Archive path if successfully generated, None otherwise.
+    """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(datetime_str)
+    try:
+        # Create versions directory if it doesn't exist (cross-platform)
+        if not os.path.exists('versions'):
+            os.makedirs('versions')
+        
+        # Use tar command to create archive
+        local('tar -cvzf versions/{} web_static'.format(file_name))
+        return "versions/{}".format(file_name)
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -21,19 +44,42 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
+    
     try:
+        # Upload the archive to /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1]
-        file_name_noext = file_name.split('.')[0]
-        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
-        run('sudo mkdir -p {}'.format(new_folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
-        run('sudo rm -rf {}web_static'.format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
+        
+        # Extract filename from path (handle both / and \ separators)
+        file_name = os.path.basename(archive_path)
+        # Remove .tgz extension to get folder name
+        file_name_noext = file_name.replace('.tgz', '')
+        
+        # Create release directory path
+        release_path = '/data/web_static/releases/{}/'.format(file_name_noext)
+        
+        # Create the release directory
+        run('mkdir -p {}'.format(release_path))
+        
+        # Uncompress the archive to the release directory
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, release_path))
+        
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(file_name))
+        
+        # Move contents from web_static subdirectory to release directory
+        run('mv {}web_static/* {}'.format(release_path, release_path))
+        
+        # Remove the now-empty web_static subdirectory
+        run('rm -rf {}web_static'.format(release_path))
+        
+        # Delete the current symbolic link
+        run('rm -rf /data/web_static/current')
+        
+        # Create new symbolic link to the new release
+        run('ln -s {} /data/web_static/current'.format(release_path))
+        
         print("New version deployed!")
         return True
+        
     except Exception:
         return False
